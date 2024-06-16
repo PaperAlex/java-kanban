ea28f6be25aeb3de1eb2acbd0f3ba31dae65545a
Alex Choi <cheng1234@yandex.ru>
2024-06-16 22:48:20 +0300

    Техническое задание проекта №6 ver 1

--- src/test/managerTest/InMemoryHistoryManagerTest.java
+++ src/test/managerTest/InMemoryHistoryManagerTest.java
@@ -6,11 +6,9 @@ import model.Epic;
 import model.Status;
 import model.Subtask;
 import model.Task;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class InMemoryHistoryManagerTest {
